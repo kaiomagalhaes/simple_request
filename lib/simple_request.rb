cf120cfb6b327d9ec0ffeea21cb08eefb9a20606
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-10-31 12:30:27 -0300

    remove debug require from simple_request.rb

--- lib/simple_request.rb
+++ lib/simple_request.rb
@@ -4,18 +4,17 @@ require_relative "simple_request/version"
 
 require "net/http"
 require "json"
-require "debug"
 require_relative "get"
 require_relative "post"
 
 module SimpleRequest
   class Error < StandardError; end
 
-  def self.get(url, params = {})
-    Get.new(url, params).call
+  def self.get(url, configuration = {})
+    Get.new(url, configuration).call
   end
 
-  def self.post(url, params = {})
-    Post.new(url, params).call
+  def self.post(url, configuration = {})
+    Post.new(url, configuration).call
   end
 end
